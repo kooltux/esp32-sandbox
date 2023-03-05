85ae027c8eb0f8db0069d2da945dd3d2130793fb
Stephane Desneux <stephane.desneux@iot.bzh>
2023-03-05 17:41:12 +0100

    blinky: support S2 and C3
    Change-Id: Ie0180cadb6167dbe96aea8ca825ea0cda1aaa165
    Signed-off-by: Stephane Desneux <stephane.desneux@iot.bzh>

--- blinky/src/Blink.cpp
+++ blinky/src/Blink.cpp
@@ -7,7 +7,8 @@
 #include <Arduino.h>
 
 // Set LED_BUILTIN if it is not defined by Arduino framework
-#define ledPin 23
+//#define ledPin 7  // C3
+#define ledPin 15 // S2
 
 void setup()
 {
@@ -19,15 +20,23 @@ void setup()
   Serial.printf("entering main loop...\n");
 }
 
+#define DEFAULT_DELAY 1000
+double period = DEFAULT_DELAY;
+
 void loop()
 {
-  Serial.printf(".");
+  Serial.printf("%f\n", period);
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(ledPin, HIGH);
   // wait for a second
-  delay(1000);
+  delay((int)period);
   // turn the LED off by making the voltage LOW
   digitalWrite(ledPin, LOW);
   // wait for a second
-  delay(1000);
+  delay((int)period);
+
+  period /= (1 + period / DEFAULT_DELAY);
+
+  if (period < 20)
+    period = DEFAULT_DELAY;
 }
